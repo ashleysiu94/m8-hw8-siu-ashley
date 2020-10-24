211fce1e731a69275c6cad8d5a0f2b08117fa235
Ashley Siu <ashley.c.siu@gmail.com>
2020-10-23 22:56:46 -0700

    adds toggle to nav

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // It is always helpful to use comments in your code!
+
+var subMenu = document.getElementsByClassName("sub-menu");
+var mainMenu = document.getElementsByClassName("main-menu-labels");
+
+
+//toggle for products
+function toggleSubMenuReveal1 () {;
+  subMenu[0].classList.toggle("show");
+}
+
+mainMenu[0].addEventListener("mouseenter", toggleSubMenuReveal1);
+mainMenu[0].addEventListener("mouseleave", toggleSubMenuReveal1);
+
+//toggle for services
+function toggleSubMenuReveal2 () {;
+  subMenu[1].classList.toggle("show");
+}
+
+mainMenu[1].addEventListener("mouseenter", toggleSubMenuReveal2);
+mainMenu[1].addEventListener("mouseleave", toggleSubMenuReveal2);
+
+//toggle for company
+function toggleSubMenuReveal3 () {;
+  subMenu[2].classList.toggle("show");
+}
+
+mainMenu[2].addEventListener("mouseenter", toggleSubMenuReveal3);
+mainMenu[2].addEventListener("mouseleave", toggleSubMenuReveal3);
